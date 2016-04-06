29cf74a538b7fba3175019b21bc9bd93a463f7fe
Alexander Clouter <alex@digriz.org.uk>
2016-04-06 19:01:20 +0100

    move to bswap_{16,32,64}

--- ops/bswap.c
+++ ops/bswap.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
-#include <endian.h>
+#include <byteswap.h>
 #include <err.h>
 #include <assert.h>
 
@@ -17,17 +17,17 @@ void bswap(struct data *data)
 	case 2:
 		*d16 += offset;
 		for (; offset < data->numrec; offset++)
-			d16[offset] = be16toh(d16[offset]);
+			d16[offset] = bswap_16(d16[offset]);
 		break;
 	case 4:
 		*d32 += offset;
 		for (; offset < data->numrec; offset++)
-			d32[offset] = be32toh(d32[offset]);
+			d32[offset] = bswap_32(d32[offset]);
 		break;
 	case 8:
 		*d64 += offset;
 		for (; offset < data->numrec; offset++)
-			d64[offset] = be64toh(d64[offset]);
+			d64[offset] = bswap_64(d64[offset]);
 		break;
 	default:
 		warnx("unsupported size %u", data->reclen);
