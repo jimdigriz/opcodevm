126461800e142de697c91d8c8694e044448ca8ef
Alexander Clouter <alex@digriz.org.uk>
2016-04-07 12:44:03 +0100

    bswap, add the other widths

--- code/bswap.c
+++ code/bswap.c
@@ -7,7 +7,17 @@ void bswap(struct op *ops, struct data *data, ...)
 {
 	uint64_t offset = 0;
 
-	ops->u32(&offset, data);
+	switch (data->reclen) {
+	case 2:
+		ops->u16(&offset, data);
+		break;
+	case 4:
+		ops->u32(&offset, data);
+		break;
+	case 8:
+		ops->u64(&offset, data);
+		break;
+	}
 
 	assert(offset == data->numrec);
 }
